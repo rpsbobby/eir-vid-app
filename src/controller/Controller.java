34506581ef573ed7734ed7768ab065e6565a22bc
Ingrid <ingrid.meccas@gmail.com>
2022-12-19 13:56:40 +0000

    Addition of Controller structure

---
+++ src/controller/Controller.java
@@ -0,0 +1,28 @@
+package controller;
+
+/**
+ *
+ * @author Ingrid Castro 2020341
+ */
+public abstract class Controller {
+
+    
+
+    public Controller() {
+    }
+
+    public void setUser() {
+    }
+
+    public Object getUser() {
+        return null;
+    }
+
+    public Object getMostRentedMovies() {
+        return null;
+    }
+
+    public Object getMovies(){
+       return null;
+    }
+}
