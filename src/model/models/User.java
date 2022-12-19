52d297931b98ef8bc5d84d4ca55ac3b2994aecf5
Ingrid <ingrid.meccas@gmail.com>
2022-12-19 21:32:31 +0000

    User implementation

--- src/model/models/User.java
+++ src/model/models/User.java
@@ -1,6 +1,8 @@
 package model.models;
 
+import java.util.List;
 import main.Constants;
+import model.exceptions.MovieAlreadyRentedException;
 import model.services.Rental;
 
 /**
@@ -19,5 +21,29 @@ public class User {
         this.password = password;
         // every user has distinct rental service
         this.rentedMovies = new Rental(Constants.USER_RENTAL_DURATION);
-    }    
+    }
+
+    public Movie rentMovie(Movie movie, long interval) throws MovieAlreadyRentedException {
+        if(rentedMovies.getRentedMovies() != null){
+            if(rentedMovies.getRentedMovies().contains(movie)) throw new MovieAlreadyRentedException("This movie is already rented");
+        }
+        return rentedMovies.rent(movie, interval);
+    }
+
+    public List<Movie> getRentedMovies() {
+         return rentedMovies.getRentedMovies();
+    }
+
+    public boolean validatePassword(String input) {
+        return password.equals(input);
+    }
+
+    public Rental getRental() {
+        return rentedMovies;
+    }
+
+    @Override
+    public String toString() {
+        return email;
+    }
 }
