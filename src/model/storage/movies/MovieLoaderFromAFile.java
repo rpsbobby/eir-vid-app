b613fba0278f802dbc56c972d562e0ec7fb3b3aa
Robert Szlufik <robert.szlufik123@gmail.com>
2022-12-19 19:29:37 +0000

    storage in memory

---
+++ src/model/storage/movies/MovieLoaderFromAFile.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.storage.movies;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import model.models.Movie;
+
+/**
+ *
+ * @author rober
+ */
+public class MovieLoaderFromAFile {
+
+ 
+
+    public MovieLoaderFromAFile() {
+    }
+
+    public Map<Integer, Movie> getAllMovies() {
+        Map<Integer, Movie> movies = new HashMap<>();
+        int index = 1;
+        try {
+            String entry;
+            String[] info;
+
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("src".concat("\\").concat("cct").concat("\\").concat("Movie_Metadata_Edited.csv")));
+            // skip first line
+            entry = bufferedReader.readLine();
+            while((entry = bufferedReader.readLine() )!= null) {
+                info = entry.split(",");
+                movies.put(index, new Movie(info[0],Double.parseDouble(info[1])));
+                index++;
+                // allow 300 movies to be fetched from the file, comas in those titles were removed
+                if (movies.size() > 300) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+
+}
